f4861eb19f27e49d14e7a77ed63264b1182ce65c
KingDanone <lricardolv10@gmail.com>
2025-07-17 11:46:38 -0300

    Controller de imagens implementado e iniciando testes

---
+++ src/main/java/br/com/KingDanone/presentation/controller/ImagemController.java
@@ -0,0 +1,36 @@
+package br.com.KingDanone.presentation.controller;
+
+
+import br.com.KingDanone.business.service.ImagemService;
+import br.com.KingDanone.presentation.dto.ImagemDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/upload")
+public class ImagemController {
+
+    private final ImagemService imagemService;
+
+    public ImagemController(ImagemService imagemService) {
+        this.imagemService = imagemService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file) {
+        try {
+            ImagemDTO dto = imagemService.salavarImagem(file);
+            return ResponseEntity.ok(dto);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("error saving image");
+        }
+    }
+}
